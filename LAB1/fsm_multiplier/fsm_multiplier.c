0f29b764a2f8952a8899c1c5c6cc686aec765a26
Kyle Williamson <mgkshome7@gmail.com>
2025-02-03 00:11:19 -0500

    Fixed the errors code runs properly now

--- LAB1/fsm_multiplier/fsm_multiplier.c
+++ LAB1/fsm_multiplier/fsm_multiplier.c
@@ -22,7 +22,10 @@ void handleErrorState();
 int main()
 {
     stdio_init_all();
-    
+    while (!stdio_usb_connected()) {
+        sleep_ms(100);
+    }
+    printf("Program Start\n");
     
 
     while (true) {
@@ -50,34 +53,49 @@ void handleIdleState() {
     printf("This is a multiplier!\n------------------\n");
     printf("Enter '1' to start:\n");
     if (scanf("%d", &first_num) == 1) {
-        while (getchar() != '\n');
-        currentState = FIRST_NUM;
+        if (first_num > 0) {
+            currentState = FIRST_NUM;
+        } else {
+            printf("Invalid input.\n");
+        }
     }
 }
 
 void handleFirstNumState() {
-    printf("Enter the first number:\n");
-    scanf("%d", &first_num);
-    while (getchar() != '\n');
-    product = first_num;
-    currentState = NEXT_NUM;
-}
-
-void handleNextNumState() {
-    printf("The current product is %d\n", product);
-    print("Enter the next number:\n");
-    scanf("%d", &next_num);
-    while (getchar() != '\n');
-    product *= next_num;
-    if (product > 9999) {
-        currentState = ERROR;
+    printf("\nEnter the first number(or '0' to reset):\n");
+    if (scanf("%d", &first_num) == 1) {
+        if (first_num > 0) {
+            product = first_num;
+            currentState = NEXT_NUM;
+        } else {
+            currentState = IDLE;
+        }
     }
 }
 
+void handleNextNumState() {
+    printf("\nThe current product is %d\n", product);
+    printf("Enter the next number(or '0' to reset):\n");
+    if (scanf("%d", &next_num) == 1) {
+        if (next_num > 0) {
+            product *= next_num;
+            if (product > 9999) {
+                currentState = ERROR;
+            }
+        } else {
+            currentState = IDLE;
+        }
+    }
+    
+}
+
 void handleErrorState() {
-    printf("OVERFLOW ERROR ENTER '1' TO RESET");
+    printf("\nOVERFLOW ERROR ENTER '0' TO RESET:\n");
     if (scanf("%d", &first_num) == 1) {
-        while (getchar() != '\n');
-        currentState = IDLE;
+        if (first_num == 0) {
+            currentState = IDLE;
+        } else {
+            printf("\nINVALID INPUT\n");
+        }
     }
 }
